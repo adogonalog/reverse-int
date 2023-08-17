f970936f3dfe62adb9d6f4795dbdd92090a82075
Eskina Daria <esenova-darya@yandex.ru>
2023-08-17 11:52:03 +0400

    feat: reverse integer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
+    let array;
+    let reversed;
+    let string;
+    let joined;
+    let result;
+    let subresult='';
+
+
+    string = n.toString().replace('-','');
+    array = string.split('');
+    reversed = array.reverse();
+    joined = reversed.join('');
+
+    return joined;
+  }
   
-}
